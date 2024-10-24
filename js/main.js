1727f5bead8056d599b38bbe878d8d489d7cf54f
NinaKharitonova <ninakharitonova95@email.com>
2024-10-24 15:15:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui 
+l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine 
+che preferisce.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e 
+uali dei numeri da indovinare sono stati individuati.
+Potete usare liste, input e bottoni non stilizzati, mettendo stampe di
+ debug in console e risultato finale in un alert.
+Solo una volta finito, a piacere e facoltativamente, potete 
+aggiungete lo stile.
+*/
